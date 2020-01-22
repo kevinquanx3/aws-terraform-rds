87ab1b972df7990bf6dadc0012f644e86c24ab2f
kevinquanx3 <kevin.quan@sage.com>
2020-01-22 09:43:27 -0800

    undo change

--- main.tf
+++ main.tf
@@ -222,7 +222,7 @@ resource "aws_db_instance" "db_instance" {
   deletion_protection = "${var.enable_deletion_protection}"
 
   allocated_storage     = "${local.storage_size}"
-  #max_allocated_storage = "${var.max_storage_size}"
+  max_allocated_storage = "${var.max_storage_size}"
   storage_type          = "${var.storage_type}"
   storage_encrypted     = "${var.storage_encrypted}"
   iops                  = "${var.storage_iops}"
